5940a3b7092dcecd869cc1dd251fbbef0bbd4459
martin <martincarusso@gmail.com>
2017-06-21 03:00:02 -0300

    hecho makefile y archivos iniciales

---
+++ src/dynamics.c
@@ -0,0 +1,11 @@
+#include "dynamics.h"
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+int hola(){
+
+  printf("hola\n");
+
+  return 0;
+}
---
+++ src/md.c
@@ -0,0 +1,12 @@
+#include "stdlib.h"
+#include <time.h>
+#include <stdio.h>
+#include "dynamics.h"
+
+
+int main(int argc, char **argv) {
+
+  hola();
+
+  return 0;
+}
